0b215db324e501affcac6e0bcc4cfd2e2a96e160
JC Brand <jc@opkode.com>
2018-05-25 06:58:57 +0200

    Options are now passed in differently

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ function lodashTemplateLoader(content, map) {
      * Interpret configuration by combining query params and `lodashTemplateLoader` options
      */
 
-    var options = this.options.lodashTemplateLoader || {};
+    var options = this.options || {};
     if (_.isFunction(options)) {
         options = options.call(this, this) || {};
     }
